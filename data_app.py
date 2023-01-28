38af8f6a4da7c4470748cf2baa78a63a928b3f5a
TanzeelaSiddique <76399241+TanzeelaSiddique@users.noreply.github.com>
2023-01-28 19:48:30 +0500

    Add files via upload
    abc

---
+++ data_app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 28 16:06:52 2023
+
+@author: wajiz.pk
+"""
+import pandas as pd
+import streamlit as st
+file = r'C:/Users/wajiz.pk/OneDrive/Desktop/Billionaire.csv'
+#reading the file
+df = pd.read_csv(file)
+#find the most popular source of income
+st.header('Billionaire Dataset')
+
+all_countries = df['Country'].unique()
+#Interactivity
+c1, c2 = st.columns(2)
+selected_country = c1.selectbox('Select Your Country',all_countries)
+subset_country = df[df['Country'] == selected_country]
+Source = sorted(subset_country['Source'].unique())
+selected_source = c1.multiselect('Select source of Income',Source)
+subset_source = subset_country[subset_country['Source'].isin(selected_source)]
+main_string = '{} -Billionaries'.format(selected_country)
+c2.header(main_string)
+c2.table(subset_country)
+c2.header('Source wise info')
+c2.table(subset_source)
+
